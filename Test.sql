e59f9fefe4ea9bd22da9218c64bd925d76c11dbb
anticN <anticn@bzz.ch>
2024-02-19 14:22:08 +0100

    test connection with SQL (hardcoded)

---
+++ Test.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS IDPA_Namen;
+
+CREATE DATABASE IDPA_Namen;
+USE idpa_namen;
+
+CREATE TABLE namen(
+name varchar(255));
+
+insert into namen
+values ("Nigola"),
+		("Fernando"),
+        ("Nils");
--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 import express from 'express';
+import mysql from 'mysql';
 
 
 const app = express();
 const port = 3000;
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'idpa', 
+    password: 'IDPA2024',
+    database: 'idpa_namen'
+})
+
+connection.connect()
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send('Test for IDPA 2024');
+    connection.query('SELECT * FROM namen', (err, rows) => {
+        if (err) throw err;
+        res.send(rows);
+    });
+    //res.send(rows);
   });
 
 
